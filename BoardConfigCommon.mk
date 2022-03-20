8843704c966426afcaed71fbecbe7e8d520017de
Borstelchen <sebastian@paetzold-family.de>
2022-03-20 11:23:13 +0100

    try enforcing selinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -191,6 +191,8 @@ BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
+# BOARD_BOOTCONFIG := androidboot.selinux=enforcing
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x10008000 --ramdisk_offset 0x11000000 --tags_offset 0x10000100
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
